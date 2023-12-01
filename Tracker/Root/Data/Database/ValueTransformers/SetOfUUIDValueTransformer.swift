a9ef2aa7a964be93e085eef95eb1142cb6fb8c9f
TheABCDEngineer <TheABCDEngineer@yandex.ru>
2023-12-01 16:14:12 +0300

    SetOfUUIDValueTransformer

--- Tracker/Root/Data/Database/ValueTransformers/SetOfUUIDValueTransformer.swift
+++ Tracker/Root/Data/Database/ValueTransformers/SetOfUUIDValueTransformer.swift
@@ -11,13 +11,13 @@ final class SetOfUUIDValueTransformer: ValueTransformer {
     }
     
     override func transformedValue(_ value: Any?) -> Any? {
-        guard let set = value as? Set<Int> else { return nil }
+        guard let set = value as? Set<UUID> else { return nil }
         return try? JSONEncoder().encode(set)
     }
     
     override func reverseTransformedValue(_ value: Any?) -> Any? {
         guard let data = value as? NSData else { return nil }
-        return try? JSONDecoder().decode(Set<Int>.self, from: data as Data)
+        return try? JSONDecoder().decode(Set<UUID>.self, from: data as Data)
     }
     
     static func register() {
